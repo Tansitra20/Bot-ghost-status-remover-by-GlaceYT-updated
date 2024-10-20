2f8399599c522bc4c1f3bc828afb9317f2256224
Tansitra <143314283+Tansitra20@users.noreply.github.com>
2024-10-20 09:21:55 +0530

    Update index.js

--- index.js
+++ index.js
@@ -37,7 +37,7 @@ app.listen(port, () => {
   console.log('\x1b[36m[ SERVER ]\x1b[0m', '\x1b[32m SH : http://localhost:' + port + ' ✅\x1b[0m');
 });
 
-const statusMessages = ["🎧 Listening to private VCs", "⏰Riding Clocks","Hydro should khs"];
+const statusMessages = ["🎧 Listening to private VCs","Silxy X Hydro"];
 const statusTypes = [ 'dnd', 'idle'];
 let currentStatusIndex = 0;
 let currentTypeIndex = 0;
